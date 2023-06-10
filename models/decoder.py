3cdecc37abdfbc2c024b27df7fe78e6477fb313c
Jaydeep Rade <jrrade@iastate.edu>
2023-06-10 18:21:39 -0500

    added residual seq decoder --> try SqueezeResNet

--- models/decoder.py
+++ models/decoder.py
@@ -1,47 +1,47 @@
 import torch
-
-class Conv3D_Up(torch.nn.Module):
+from torch import nn
+class Conv3D_Up(nn.Module):
 	def __init__(self, c_in, c_out, kernel_size=4, padding=1, stride=2, dropout=None):
 		super(Conv3D_Up, self).__init__()
 		if dropout is None:	
-			self.layers = torch.nn.Sequential(
-						torch.nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
-						torch.nn.BatchNorm3d(c_out),
-						torch.nn.ReLU(),
-						torch.nn.Conv3d(c_out, c_out, kernel_size=3, padding=1, stride=1),
-						torch.nn.BatchNorm3d(c_out),
-						torch.nn.ReLU(),
+			self.layers = nn.Sequential(
+						nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
+						nn.BatchNorm3d(c_out),
+						nn.ReLU(),
+						nn.Conv3d(c_out, c_out, kernel_size=3, padding=1, stride=1),
+						nn.BatchNorm3d(c_out),
+						nn.ReLU(),
 						)
 		else:
-			self.layers = torch.nn.Sequential(
-						torch.nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
-						torch.nn.BatchNorm3d(c_out),
-						torch.nn.ReLU(),
-						torch.nn.Dropout3d(dropout),
-						torch.nn.Conv3d(c_out, c_out, kernel_size=3, padding=1, stride=1),
-						torch.nn.BatchNorm3d(c_out),
-						torch.nn.ReLU(),
-						torch.nn.Dropout3d(dropout),
+			self.layers = nn.Sequential(
+						nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
+						nn.BatchNorm3d(c_out),
+						nn.ReLU(),
+						nn.Dropout3d(dropout),
+						nn.Conv3d(c_out, c_out, kernel_size=3, padding=1, stride=1),
+						nn.BatchNorm3d(c_out),
+						nn.ReLU(),
+						nn.Dropout3d(dropout),
 						)
 
 	def forward(self, x):
 		return self.layers(x)
 	
 	
-# class Conv3D_Up(torch.nn.Module):
+# class Conv3D_Up(nn.Module):
 # 	def __init__(self, c_in, c_out, kernel_size=4, padding=1, stride=2):
 # 		super(Conv3D_Up, self).__init__()
-# 		self.layers = torch.nn.Sequential(
-# 					  torch.nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
-# 					  torch.nn.BatchNorm3d(c_out),
-# 					  torch.nn.ReLU(),
+# 		self.layers = nn.Sequential(
+# 					  nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
+# 					  nn.BatchNorm3d(c_out),
+# 					  nn.ReLU(),
 # 					  )
 
 # 	def forward(self, x):
 # 		return self.layers(x)
 
 
-class Decoder(torch.nn.Module):
+class Decoder(nn.Module):
 	def __init__(self, cfg):
 		super(Decoder, self).__init__()
 		self.cfg = cfg
@@ -50,13 +50,13 @@ class Decoder(torch.nn.Module):
 		# Layer Definition
 						
 
-		self.layer1 = torch.nn.Sequential(
-			torch.nn.Conv2d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm2d(self.c_out),
-			torch.nn.ReLU(),
-			torch.nn.Conv2d(self.c_out, self.c_out, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm2d(self.c_out),
-			torch.nn.ReLU(),
+		self.layer1 = nn.Sequential(
+			nn.Conv2d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(self.c_out),
+			nn.ReLU(),
+			nn.Conv2d(self.c_out, self.c_out, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm2d(self.c_out),
+			nn.ReLU(),
 			)
 		    # 4x4x512
 
@@ -79,9 +79,9 @@ class Decoder(torch.nn.Module):
 		# 32x32x32x16
 
 
-		self.layer5 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
-			torch.nn.Sigmoid()
+		self.layer5 = nn.Sequential(
+			nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
+			nn.Sigmoid()
 		)
 
 	def forward(self, x):
@@ -95,44 +95,42 @@ class Decoder(torch.nn.Module):
 		
 		return output.squeeze(dim=1)
 
-class ResBlock3D(torch.nn.Module):
+class ResBlock3D(nn.Module):
 	def __init__(self, c_in):
 		super(ResBlock3D, self).__init__()
-		self.layers = torch.nn.Sequential(
-			torch.nn.Conv3d(c_in, c_in, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm3d(c_in),
-			torch.nn.ReLU(),
-			torch.nn.Conv3d(c_in, c_in, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm3d(c_in),
-			torch.nn.ReLU()
+		self.layers = nn.Sequential(
+			nn.Conv3d(c_in, c_in, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm3d(c_in),
+			nn.ReLU(),
+			nn.Conv3d(c_in, c_in, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm3d(c_in),
+			nn.ReLU()
 			)
 	def forward(self, x):
-		res = x
-		x = self.layers(x)
-		x += res
-		return x
+		return x + self.layers(x)
 	
-class Conv3D_Up_Res(torch.nn.Module):
+class Conv3D_Up_Res(nn.Module):
 	def __init__(self, c_in, c_out, kernel_size=4, padding=1, stride=2, dropout=None):
 		super(Conv3D_Up_Res, self).__init__()
 		self.dropout = dropout
-		if self.dropout is not None:
-			self.dropout = torch.nn.Dropout3d(dropout)
-
-		self.Conv3D_Up = torch.nn.Sequential(
-						torch.nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
-						torch.nn.BatchNorm3d(c_out),
-						torch.nn.ReLU(),
+		
+		self.Conv3D_Up = nn.Sequential(
+						nn.ConvTranspose3d(c_in, c_out, kernel_size=kernel_size, padding=padding, stride=stride),
+						nn.BatchNorm3d(c_out),
+						nn.ReLU(),
 						)
+		if self.dropout is not None:
+			self.dropout = nn.Dropout3d(dropout)
+
 		self.ResBlock3D = ResBlock3D(c_out)
 	def forward(self, x):
+		x = self.Conv3D_Up(x)
 		if self.dropout is not None:
 			x = self.dropout(x)
-		x = self.Conv3D_Up(x)
 		x = self.ResBlock3D(x)
-		return self.layers(x)
+		return x
 
-class OnlySeq_Decoder(torch.nn.Module):
+class OnlySeq_Decoder(nn.Module):
 	def __init__(self, cfg):
 		super(OnlySeq_Decoder, self).__init__()
 		self.cfg = cfg
@@ -140,50 +138,50 @@ class OnlySeq_Decoder(torch.nn.Module):
 		self.c_out = 128 #64 #128 
 		# Layer Definition
 
-		self.layer1 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm3d(self.c_out),
-			torch.nn.ReLU()
+		self.layer1 = nn.Sequential(
+			nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm3d(self.c_out),
+			nn.ReLU()
 		)
 		if cfg.NETWORK.DROPOUT is not None:
-			self.layer1.append(torch.nn.Dropout3d(cfg.NETWORK.DROPOUT))
+			self.layer1.append(nn.Dropout3d(cfg.NETWORK.DROPOUT))
 		# 4x4x4x128
 			
-		self.ResBlock3D_1 = ResBlock3D(self.c_in)
-		if cfg.NETWORK.DROPOUT is not None:
-			self.drop1 = torch.nn.Dropout3d(cfg.NETWORK.DROPOUT)
+		self.ResBlock3D_1 = ResBlock3D(self.c_out)
+		# if cfg.NETWORK.DROPOUT is not None:
+		# 	self.drop1 = nn.Dropout3d(cfg.NETWORK.DROPOUT)
 		# 4x4x4x128
 		
-		self.c_in = 128 
-		self.c_out = 64 
-		self.layers = torch.nn.ModuleList([])	
+		# self.c_in = 128 
+		# self.c_out = 64 
+		self.layers = nn.ModuleList([])	
 		for i in range(3):
-			self.layers.append(torch.nn.Sequential([Conv3D_Up_Res(self.c_in, self.c_out, dropout=cfg.NETWORK.DROPOUT)]))
 			self.c_in = self.c_out
 			self.c_out //= 2
+			self.layers.append(Conv3D_Up_Res(self.c_in, self.c_out, dropout=cfg.NETWORK.DROPOUT))
+			
 
-
-		self.layer5 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
-			torch.nn.Sigmoid()
+		self.layer5 = nn.Sequential(
+			nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
+			nn.Sigmoid()
 		)
 
-		self.fc1 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.BatchNorm1d(1280),
-			torch.nn.ReLU()
+		self.fc1 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.BatchNorm1d(1280),
+			nn.ReLU()
 		)
 		if cfg.NETWORK.DROPOUT is not None:
-			self.fc1.append(torch.nn.Dropout(cfg.NETWORK.DROPOUT))
+			self.fc1.append(nn.Dropout(cfg.NETWORK.DROPOUT))
 
-		self.fc2 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.BatchNorm1d(1280),
-			torch.nn.ReLU()
+		self.fc2 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.BatchNorm1d(1280),
+			nn.ReLU()
 		)
 
 		if cfg.NETWORK.DROPOUT is not None:
-			self.fc2.append(torch.nn.Dropout(cfg.NETWORK.DROPOUT))
+			self.fc2.append(nn.Dropout(cfg.NETWORK.DROPOUT))
 		
 	def forward(self, x):
 		x = x.view(-1, 1280)
@@ -193,13 +191,14 @@ class OnlySeq_Decoder(torch.nn.Module):
 		x = x.view(-1, 20, 4, 4, 4)
 		x = self.layer1(x)
 		x = self.ResBlock3D_1(x)
-		x = self.layers(x)
+		for layer in self.layers:
+			x = layer(x)
 		output = self.layer5(x)
 		
 		return output.squeeze(dim=1)
 
 
-class OnlySeq_Decoder_with_4point2Mn_params(torch.nn.Module):
+class OnlySeq_Decoder_with_4point2Mn_params(nn.Module):
 	def __init__(self, cfg):
 		super(OnlySeq_Decoder_with_4point2Mn_params, self).__init__()
 		self.cfg = cfg
@@ -208,13 +207,13 @@ class OnlySeq_Decoder_with_4point2Mn_params(torch.nn.Module):
 		# Layer Definition
 						
 
-		self.layer1 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm3d(self.c_out),
-			torch.nn.ReLU()
+		self.layer1 = nn.Sequential(
+			nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm3d(self.c_out),
+			nn.ReLU()
 		)
 		if cfg.NETWORK.DROPOUT is not None:
-			self.layer1.append(torch.nn.Dropout3d(cfg.NETWORK.DROPOUT))
+			self.layer1.append(nn.Dropout3d(cfg.NETWORK.DROPOUT))
 		# 4x4x512
 
 		self.c_in = 128 #64 #128
@@ -236,27 +235,27 @@ class OnlySeq_Decoder_with_4point2Mn_params(torch.nn.Module):
 		# 32x32x32x16
 
 
-		self.layer5 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
-			torch.nn.Sigmoid()
+		self.layer5 = nn.Sequential(
+			nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
+			nn.Sigmoid()
 		)
 
-		self.fc1 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.BatchNorm1d(1280),
-			torch.nn.ReLU()
+		self.fc1 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.BatchNorm1d(1280),
+			nn.ReLU()
 		)
 		if cfg.NETWORK.DROPOUT is not None:
-			self.fc1.append(torch.nn.Dropout(cfg.NETWORK.DROPOUT))
+			self.fc1.append(nn.Dropout(cfg.NETWORK.DROPOUT))
 
-		self.fc2 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.BatchNorm1d(1280),
-			torch.nn.ReLU()
+		self.fc2 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.BatchNorm1d(1280),
+			nn.ReLU()
 		)
 
 		if cfg.NETWORK.DROPOUT is not None:
-			self.fc2.append(torch.nn.Dropout(cfg.NETWORK.DROPOUT))
+			self.fc2.append(nn.Dropout(cfg.NETWORK.DROPOUT))
 		
 	def forward(self, x):
 		x = x.view(-1, 1280)
@@ -273,7 +272,7 @@ class OnlySeq_Decoder_with_4point2Mn_params(torch.nn.Module):
 		
 		return output.squeeze(dim=1)
 
-class OnlySeq_Decoder_previous_4mn_params(torch.nn.Module):
+class OnlySeq_Decoder_previous_4mn_params(nn.Module):
 	def __init__(self, cfg):
 		super(OnlySeq_Decoder_previous_4mn_params, self).__init__()
 		self.cfg = cfg
@@ -282,10 +281,10 @@ class OnlySeq_Decoder_previous_4mn_params(torch.nn.Module):
 		# Layer Definition
 						
 
-		self.layer1 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
-			torch.nn.BatchNorm3d(self.c_out),
-			torch.nn.ReLU()
+		self.layer1 = nn.Sequential(
+			nn.Conv3d(self.c_in, self.c_out, kernel_size=3, stride=1, padding=1),
+			nn.BatchNorm3d(self.c_out),
+			nn.ReLU()
 		)
 		# 4x4x512
 
@@ -308,19 +307,19 @@ class OnlySeq_Decoder_previous_4mn_params(torch.nn.Module):
 		# 32x32x32x16
 
 
-		self.layer5 = torch.nn.Sequential(
-			torch.nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
-			torch.nn.Sigmoid()
+		self.layer5 = nn.Sequential(
+			nn.Conv3d(self.c_out, 1, kernel_size=3, stride=1, padding=1),
+			nn.Sigmoid()
 		)
 
-		self.fc1 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.ReLU()
+		self.fc1 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.ReLU()
 		)
 
-		self.fc2 = torch.nn.Sequential(
-			torch.nn.Linear(1280, 1280),
-			torch.nn.ReLU()
+		self.fc2 = nn.Sequential(
+			nn.Linear(1280, 1280),
+			nn.ReLU()
 		)
 
 	def forward(self, x):
